eead069a5c04ce04c41971beabc0f78e47a57fb1
Tomy <kazushige.tominaga@mkt-apps.com>
2017-07-31 23:24:44 +0900

    up

--- 5-5.c
+++ 5-5.c
@@ -1,11 +1,51 @@
 #include <stdio.h>
 
-void strncpy(char *s, char *t, int n)
+/* t を s にｎ文字コピーする */
+void my_strncpy(char *s, char *t, int n)
 {
-    
+    while ((*s++ = *t++) && n-- > 1)
+        ;
+
+    *s = '\0';
+}
+
+void my_strncat(char *s, char *t, int n)
+{
+    while (*s != '\0')
+        s++;
+
+    int i;
+    for (i = 0; i < n; i++)
+        *s++ = *t++;
+
+    *s = '\0';
+}
+
+int my_strncmp(char *s, char *t, int n)
+{
+    int i;
+    for (i = 0; i < n; i++, s++, t++) {
+        if (*s != *t)
+            return *s - *t;
+    }
+
+    return 0;
 }
 
 int main()
 {
-    
+    char *s;
+    char t[100] = "hoge";
+
+    my_strncpy(s, t, 3);
+
+    printf("%s\n", s);
+
+    char s2[1] = "h";
+    my_strncat(s2, "ogefuga", 3);
+    printf("%s\n", s2);
+
+    printf("%d\n", my_strncmp("hogefuga", "hogehoge", 5));
+
+    return 0;
 }
