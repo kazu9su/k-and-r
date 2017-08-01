726afae543667bb82a8b4faaa1b7547d39d3de87
Tomy <kazushige.tominaga@mkt-apps.com>
2017-08-02 00:07:53 +0900

    up

---
+++ 5-6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int my_getline(char *s, int lim);
+
+int main()
+{
+    char *s;
+
+    printf("%d\n", my_getline(s, 1000));
+
+    return 0;
+}
+
+int my_getline(char *s, int lim)
+{
+    int c, i;
+
+    for (i=0; 0 < lim-1 && (c=getchar()) != EOF && c!='\n'; i++)
+        *s++ = c;
+        if (c == '\n') {
+            *s++ = c;
+            ++i;
+    }
+    *s = '\0';
+
+    return i;
+}
